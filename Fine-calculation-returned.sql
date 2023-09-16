25989ae2816bd6d0a55cd30675ee93b06ba2a112
Md. Mubassir Ahsan <mahsandu@users.noreply.github.com>
2023-09-17 03:29:27 +0600

    updated

--- Fine-calculation-returned.sql
+++ Fine-calculation-returned.sql
@@ -5,9 +5,14 @@ SELECT
     DueDate,
     ReturnDate,
     DueDays,
-    Holidays,
-    (DueDays - Holidays) AS FineDays,
-    ((DueDays - Holidays) * 2) AS TotalFine,
+    CASE
+        WHEN DueDays - Holidays < 0 THEN 0
+        ELSE DueDays - Holidays
+    END AS FineDays,
+    CASE
+        WHEN DueDays - Holidays < 0 THEN 0
+        ELSE (DueDays - Holidays) * 2
+    END AS ActualFine,
     AmountOutStanding,
     Amount,
     status,
@@ -21,7 +26,7 @@ FROM (
         DATE_FORMAT(o.date_due, '%d-%m-%Y') AS DueDate,
         DATE_FORMAT(o.returndate, '%d-%m-%Y') AS ReturnDate,
         CAST(DATEDIFF(DATE_FORMAT(o.returndate, '%Y-%m-%d'), DATE_FORMAT(o.date_due, '%Y-%m-%d')) - 7 AS SIGNED) AS DueDays,
-        CAST((DATEDIFF(DATE_FORMAT(o.returndate, '%Y-%m-%d'), DATE_FORMAT(o.date_due, '%Y-%m-%d')) -7) / 7 AS SIGNED)AS Holidays,
+        CAST((DATEDIFF(DATE_FORMAT(o.returndate, '%Y-%m-%d'), DATE_FORMAT(o.date_due, '%Y-%m-%d')) - 7) / 7 AS SIGNED) AS Holidays,
         CAST(a.amountoutstanding AS SIGNED) AS AmountOutStanding,
         CAST(a.amount AS SIGNED) AS Amount,
         a.status AS status,
@@ -36,10 +41,9 @@ FROM (
         )
         LEFT JOIN borrowers b ON b.borrowernumber = a.borrowernumber
     WHERE
-        a.amountoutstanding > 0 AND
-        a.status='RETURNED'
+        a.amountoutstanding > 0
+        AND a.status = 'RETURNED'
         -- Replace with the itemnumber you want to filter by
     GROUP BY
         a.borrowernumber
 ) AS DerivedTable;
-
