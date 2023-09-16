783eea30debf2a18df93993bcb8da48aa85e06cf
Md. Mubassir Ahsan <mahsandu@users.noreply.github.com>
2023-09-17 00:57:27 +0600

    finished furnishing returned and unreturned views sql

--- Fine-calculation-returned.sql
+++ Fine-calculation-returned.sql
@@ -5,15 +5,14 @@ SELECT
     DueDate,
     ReturnDate,
     DueDays,
-    1 AS TodayInCalc,
-    7 AS GracePeriod,
     Holidays,
-    (DueDays - (Holidays + 8)) AS FineDays,
-    ((DueDays - (Holidays + 8)) * 2) AS TotalFine,
-    CAST(Amount AS SIGNED) AS Amount,
+    (DueDays - Holidays) AS FineDays,
+    ((DueDays - Holidays) * 2) AS TotalFine,
+    AmountOutStanding,
+    Amount,
     status,
-    timestamp,
-    credit_type_code
+    credit_type_code,
+    DATE_FORMAT(CURDATE(), '%d-%m-%Y') AS Today
 FROM (
     SELECT
         b.cardnumber AS cardnumber,
@@ -21,12 +20,11 @@ FROM (
         a.itemnumber AS itemnumber,
         DATE_FORMAT(o.date_due, '%d-%m-%Y') AS DueDate,
         DATE_FORMAT(o.returndate, '%d-%m-%Y') AS ReturnDate,
-        CAST(DATEDIFF(o.returndate, o.date_due) AS SIGNED) AS DueDays,
-        CAST(SUM(DATEDIFF(o.returndate, o.date_due) /7) AS SIGNED) AS HoliDays,
+        CAST(DATEDIFF(DATE_FORMAT(o.returndate, '%Y-%m-%d'), DATE_FORMAT(o.date_due, '%Y-%m-%d')) - 7 AS SIGNED) AS DueDays,
+        CAST((DATEDIFF(DATE_FORMAT(o.returndate, '%Y-%m-%d'), DATE_FORMAT(o.date_due, '%Y-%m-%d')) -7) / 7 AS SIGNED)AS Holidays,
         CAST(a.amountoutstanding AS SIGNED) AS AmountOutStanding,
         CAST(a.amount AS SIGNED) AS Amount,
         a.status AS status,
-        a.timestamp AS timestamp,
         a.credit_type_code AS credit_type_code
     FROM
         (
@@ -38,9 +36,10 @@ FROM (
         )
         LEFT JOIN borrowers b ON b.borrowernumber = a.borrowernumber
     WHERE
-        a.amountoutstanding > 0
-        AND STATUS ='RETURNED'
-        AND a.itemnumber = '12780' -- Replace with the itemnumber you want to filter by
+        a.amountoutstanding > 0 AND
+        a.status='RETURNED'
+        -- Replace with the itemnumber you want to filter by
     GROUP BY
         a.borrowernumber
 ) AS DerivedTable;
+
